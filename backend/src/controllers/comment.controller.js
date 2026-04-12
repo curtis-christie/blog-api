3f5743ba0922292b480e2ba8f9d32a9377bf697b
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2026-04-12 11:34:53 -0400

    comment routes/controller setup

---
+++ backend/src/controllers/comment.controller.js
@@ -0,0 +1,18 @@
+// getComment /comments/:commentId
+async function getComment(req, res, next) {
+  // logic
+}
+// getPostComments /comments/post/:postId
+async function getPostComments(req, res, next) {
+  // logic
+}
+// createComment /comments
+async function createComment(req, res, next) {
+  //logic
+}
+// deleteComment /comments/:commentId
+async function deleteComment(req, res, next) {
+  //logic
+}
+
+export { getComment, getPostComments, createComment, deleteComment };
--- backend/src/routes/comments.routes.js
+++ backend/src/routes/comments.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
+import {
+  createComment,
+  deleteComment,
+  getComment,
+  getPostComments,
+} from "../controllers/comment.controller.js";
 
 const commentRouter = Router();
 
 // get comment by id
+commentRouter.get("/:commentId", getComment);
 // get comments by postId
+commentRouter.get("/post/:postId", getPostComments);
 // create comment
+commentRouter.post("/", createComment);
 // delete comment
+commentRouter.delete("/:commentId", deleteComment);
 
 export default commentRouter;
