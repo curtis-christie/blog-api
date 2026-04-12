9ab402573a373a11c65ef0e6710291ef4f263f93
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2026-04-12 10:52:06 -0400

    comments route start

---
+++ backend/src/routes/comments.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const commentRouter = Router();
+
+// get comment by id
+// get comments by postId
+// create comment
+// delete comment
+
+export default commentRouter;
--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import healthRoutes from "./health.routes.js";
 import postsRouter from "./posts.routes.js";
 import authRouter from "./auth.routes.js";
+import commentRouter from "./comments.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/health", healthRoutes);
 router.use("/posts", postsRouter);
+router.use("/comments", commentRouter);
 
 export default router;
